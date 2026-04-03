15cb1bcaa0f202a1c50713eb49312bd7632cde82
Aaron Wright <aaron.c.wright@icloud.com>
2026-04-02 23:20:35 -0500

    Update INC file

--- Hello World/Hello World.asm
+++ Hello World/Hello World.asm
@@ -2,9 +2,7 @@
 
 .include "../6502.inc"
 
-.segment "CODE"
-
-Chrout := $A000       
+.segment "CODE"     
 
 BasicStartup: .byte $0A, $08, $0A, $00, $9E, $32, $30, $36, $30, $00, $00, $00    ; BASIC = 10 SYS 2060 
 
